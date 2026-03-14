6e7e6e890f968e59a37665282f215bb8ffdc06d2
Jaime Silva <jaime.silva@cobli.co>
2026-03-14 19:04:18 -0300

    primeiro commit da estrutura base do projeto com as dependencias para seu funcionamento

---
+++ src/main/java/com/github/jaimejean/catapi/CatApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaimejean.catapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jaimejean/catapi/CatApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaimejean.catapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
